7188a3ea61b82646196565770a910b350ca4a225
nicholasruggeri <ruggerinicholas@gmail.com>
2016-04-13 14:29:54 +0200

    test structured message

--- index.js
+++ index.js
@@ -27,6 +27,55 @@ function sendTextMessage(sender, text) {
     });
 }
 
+function sendGenericMessage(sender) {
+  var messageData = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "First card",
+          "subtitle": "Element #1 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+          "buttons": [{
+            "type": "web_url",
+            "url": "https://www.messenger.com/",
+            "title": "Web url"
+          }, {
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for first element in a generic bubble",
+          }],
+        },{
+          "title": "Second card",
+          "subtitle": "Element #2 of an hscroll",
+          "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+          "buttons": [{
+            "type": "postback",
+            "title": "Postback",
+            "payload": "Payload for second element in a generic bubble",
+          }],
+        }]
+      }
+    }
+  };
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -54,7 +103,11 @@ app.post('/', function (req, res) {
         if (event.message && event.message.text) {
             var text = event.message.text;
             // Handle a text message from this sender
-            sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+            // sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+            if (text === 'Generic') {
+                sendGenericMessage(sender);
+                continue;
+            }
         }
 
     }
