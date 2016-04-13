740f0cbf83ee5a34ed60f840a812357d7be15467
nicholasruggeri <ruggerinicholas@gmail.com>
2016-04-13 17:20:33 +0200

    refactor events

--- events/events.js
+++ events/events.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 module.exports = {
 
-    sendTextMessage: function(token, sender, text) {
+    sendTextMessage: (token, sender, text) => {
         var messageData = {
             text: text
         }
@@ -18,7 +18,48 @@ module.exports = {
                 },
                 message: messageData,
             }
-        }, function(error, response, body) {
+        }, (error, response, body) => {
+            if (error) {
+                console.log('Error sending message: ', error);
+            } else if (response.body.error) {
+                console.log('Error: ', response.body.error);
+            }
+        });
+    },
+
+
+    sendGenericMessage: (token, sender) => {
+        var messageData = {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "generic",
+                    "elements": [{
+                        "title": "Batman VS Superman",
+                        "subtitle": "17:30 - 19:30 - 21:30 - 23:00",
+                        "image_url": "https://s.yimg.com/ny/api/res/1.2/gyFqKB85n5rFl4e1SpvJDg--/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAwO2lsPXBsYW5l/http://l.yimg.com/cd/resizer/2.0/FIT_TO_WIDTH-w1280/08d16d4567f303c46f16a66041eca2f620352f4b.jpg",
+                    },{
+                        "title": "Kung Fu Panda 3",
+                        "subtitle": "17:00 - 19:00 - 21:00",
+                        "image_url": "http://valecenter.it/wp-content/uploads/2016/03/kung-fu-panda-3-poster-full.jpg",
+                    }]
+                }
+            }
+        };
+
+        request({
+            url: 'https://graph.facebook.com/v2.6/me/messages',
+            qs: {
+                access_token: token
+            },
+            method: 'POST',
+            json: {
+                recipient: {
+                    id: sender
+                },
+                message: messageData,
+            }
+        }, (error, response, body) => {
             if (error) {
                 console.log('Error sending message: ', error);
             } else if (response.body.error) {
@@ -26,6 +67,8 @@ module.exports = {
             }
         });
     }
-
-
 }
+
+
+
+
--- index.js
+++ index.js
@@ -1,67 +1,13 @@
 
-var util = require('util');
-
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    request = require('request');
+var util        = require('util'),
+    express     = require('express'),
+    bodyParser  = require('body-parser'),
+    request     = require('request');
 
 var events = require('./events/events');
 
 var app = express(),
     token = process.env.FB_TOKEN;
-
-
-
-function sendGenericMessage(sender) {
-  var messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [{
-          "title": "Batman VS Superman",
-          "subtitle": "17:30 - 19:30 - 21:30 - 23:00",
-          "image_url": "https://s.yimg.com/ny/api/res/1.2/gyFqKB85n5rFl4e1SpvJDg--/YXBwaWQ9aGlnaGxhbmRlcjtzbT0xO3c9ODAwO2lsPXBsYW5l/http://l.yimg.com/cd/resizer/2.0/FIT_TO_WIDTH-w1280/08d16d4567f303c46f16a66041eca2f620352f4b.jpg",
-          // "buttons": [
-          //     {
-          //       "type": "web_url",
-          //       "url": "https://www.messenger.com/",
-          //       "title": "Web url"
-          //     }, {
-          //       "type": "postback",
-          //       "title": "Postback",
-          //       "payload": "Payload for first element in a generic bubble",
-          //     }
-          // ],
-        },{
-          "title": "Kung Fu Panda 3",
-          "subtitle": "17:00 - 19:00 - 21:00",
-          "image_url": "http://valecenter.it/wp-content/uploads/2016/03/kung-fu-panda-3-poster-full.jpg",
-          // "buttons": [{
-          //   "type": "postback",
-          //   "title": "Postback",
-          //   "payload": "Payload for second element in a generic bubble",
-          // }],
-        }]
-      }
-    }
-  };
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
-}
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -91,7 +37,7 @@ app.post('/', function (req, res) {
             var text = event.message.text;
             // Handle a text message from this sender
             if (text === 'The Space Silea') {
-                sendGenericMessage(sender);
+                events.sendGenericMessage(token, sender);
                 continue;
             } else {
                 events.sendTextMessage(token, sender, "Theater not found, sorry...");
@@ -105,4 +51,4 @@ app.post('/', function (req, res) {
 
 app.listen(process.env.PORT);
 
-console.log('Magic happens on port ' + process.env.PORT);
+console.log('Shit happens on port ' + process.env.PORT);
