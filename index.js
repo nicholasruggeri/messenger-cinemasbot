238800391f020a581a5494b52656075de49db4de
nicholasruggeri <ruggerinicholas@gmail.com>
2016-04-13 15:50:48 +0200

    add test console.dir

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.post('/', function (req, res) {
         var event = req.body.entry[0].messaging[i];
         var sender = event.sender.id;
 
-        console.log('event', event)
+        console.dir('event', event)
 
         if (event.message && event.message.text) {
             var text = event.message.text;
