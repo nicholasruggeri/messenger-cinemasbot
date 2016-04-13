2160f721f016534e95aa064e1cdaf4b55b96f00b
nicholasruggeri <ruggerinicholas@gmail.com>
2016-04-13 12:08:36 +0200

    test

--- index.js
+++ index.js
@@ -49,11 +49,11 @@ app.post('/', function (req, res) {
     console.log('req', req)
 
     var messaging_events = req.body.entry[0].messaging;
-    for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i];
-        sender = event.sender.id;
+    for (var i = 0; i < messaging_events.length; i++) {
+        var event = req.body.entry[0].messaging[i];
+        var sender = event.sender.id;
         if (event.message && event.message.text) {
-            text = event.message.text;
+            var text = event.message.text;
             // Handle a text message from this sender
             sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
         }
