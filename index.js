e15af348ece9d7dc2b1643bf4b728b98af9dee86
nicholasruggeri <ruggerinicholas@gmail.com>
2016-04-13 11:13:59 +0200

    test

--- index.js
+++ index.js
@@ -12,14 +12,28 @@ var app = express();
 // Node.js Example
 app.get('/', function (req, res) {
 
-    console.log('res', res)
-
     if (req.query['hub.verify_token'] === "majora-2001") {
         res.send(req.query['hub.challenge']);
     } else {
         res.send('Error, wrong validation token');
     }
 
+});
+
+app.post('/', function (req, res) {
+
+    console.log('res', res)
+
+    messaging_events = req.body.entry[0].messaging;
+    for (i = 0; i < messaging_events.length; i++) {
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+        if (event.message && event.message.text) {
+            text = event.message.text;
+            // Handle a text message from this sender
+        }
+    }
+    res.sendStatus(200);
 });
 
 
