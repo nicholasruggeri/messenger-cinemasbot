b7b2d278e2d04f47a838c6e57e492ce2658dbc89
nicholasruggeri <ruggerinicholas@gmail.com>
2016-04-13 12:41:04 +0200

    add var env

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
 
-// Dipendenze terze parti
 var express = require('express'),
     bodyParser = require('body-parser'),
     request = require('request');
 
-
-var app = express();
-
-var token = "CAAQX4MNgOq8BALoqRGUGDu3QJR18jiU0musa0HQTngkCP0JPgwpg3FWXxJ7RUDar5AnLViQHW345cwJawwEgztCvATnIoNLgVEaZBbIdxXP2ZA6ZBl9tlnlFZBZCLRJmaJavDDB6ckklxnukdFh7rahjQMhORa8YGR9L52bE4vfpWkL7LJwiHW5uL69U6EnEZD";
+var app = express(),
+    token = process.env.FB_TOKEN;
 
 function sendTextMessage(sender, text) {
-  var messageData = {
-    text:text
-  }
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
+    var messageData = {
+        text: text
     }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
 }
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -48,19 +45,24 @@ app.post('/', function (req, res) {
     console.log('req', req)
 
     var messaging_events = req.body.entry[0].messaging;
+
     for (var i = 0; i < messaging_events.length; i++) {
+
         var event = req.body.entry[0].messaging[i];
         var sender = event.sender.id;
+
         if (event.message && event.message.text) {
             var text = event.message.text;
             // Handle a text message from this sender
             sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
         }
+
     }
+
     res.sendStatus(200);
+
 });
 
-
-
 app.listen(process.env.PORT);
+
 console.log('Magic happens on port ' + process.env.PORT);
