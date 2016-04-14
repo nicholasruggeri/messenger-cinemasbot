fa14c5bd852ca1896c65c70f26b84e7c042b631d
nicholasruggeri <ruggerinicholas@gmail.com>
2016-04-14 23:59:57 +0200

    test

--- services/services.js
+++ services/services.js
@@ -1,30 +1,31 @@
 var request = require('request'),
     cheerio = require('cheerio');
 
-var helpers = require('../helpers/helpers');
-
 module.exports = {
 
     getCinema: function(location, callback){
-        var googleUrl = 'http://www.google.com/movies?near='+location;
+
+        let googleUrl = 'http://www.google.com/movies?near='+location;
+
         request(googleUrl, function(error, response, html){
+
             if(!error){
+
                 var $ = cheerio.load(html);
                 var list_theaters = [];
-                $('.theater .desc .name a').each(function(index){
-                    var element = {};
-                    var data = $(this);
-                    var name = data.text(),
-                        info = data.parent().parent().find('.info').text(),
-                        link = data.attr('href');
-                    element = name;
-                    list_theaters.push(element);
+
+                $('.theater .desc .name a').each(function(){
+
+                    let theater_name = $(this).text();
+                    list_theaters.push(theater_name);
+
                 });
-                // console.log(list_theaters)
+
                 if (typeof callback == "function")
                     return callback(list_theaters);
                 else
                     return list_theaters;
+
             } else {
                 console.log("ERROR GETCINEMA", err); return;
             }
@@ -44,7 +45,7 @@ module.exports = {
                     var movies = [];
                     var movies_promise = [];
 
-                    $('.theater .desc .name a').each(function(index){
+                    $('.theater .desc .name a').each(function(){
                         var text = $(this).text()
                         if (text == theater){
                             var data = $(this);
@@ -78,15 +79,11 @@ module.exports = {
 
                                 }).then((data) => {
 
-
-
                                 }).catch((response) => {
                                     console.log('error', response)
                                 }));
 
                             });
-
-
 
                         }
                     });
@@ -101,34 +98,7 @@ module.exports = {
 
         })
 
-    },
-
-    getTimes: function(location, theater, movie, callback){
-        var googleUrl = 'http://www.google.com/movies?near='+location;
-        request(googleUrl, function(error, response, html){
-            if(!error){
-                var $ = cheerio.load(html);
-                $('.theater .desc .name a').each(function(){
-                    var text = $(this).text()
-                    if (text == theater){
-                        var data = $(this);
-                        data.parent().parent().siblings('.showtimes').find('.movie').each(function(){
-                            var text = $(this).find('.name').text();
-                            if (text == movie){
-                                var data = $(this);
-                                var movieTimes = data.find('.times').text();
-                                var responseTimes = "The show times for " + decodeURI(movie) + " are: " + movieTimes + ". " + helpers.textResponse.beer;
-                                if (typeof callback == "function")
-                                    return callback(responseTimes);
-                                else
-                                    return responseTimes;
-                            }
-                        });
-                    }
-                });
-            } else {
-                console.log("ERROR GETTIMES", err); return;
-            }
-        });
     }
+
+
 }
