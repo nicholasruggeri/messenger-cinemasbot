42669d72ecb69699c2a2cf2b1344829bb324c249
nicholasruggeri <ruggerinicholas@gmail.com>
2016-06-03 14:15:42 +0200

    refactoring botus

--- index.js
+++ index.js
@@ -114,29 +114,37 @@ app.post('/', function (req, res) {
 
                 user_session[sender_id].location = coords;
 
+                console.log('user_location', user_session[sender_id].location)
+
                 events.sendTextMessage(
                     token,
                     user_session[sender_id].id,
                     "Fangtastic! I’m hunting down some theaterrrrs for you."
                 );
 
-                () => {
-                    new Promise((resolve, reject) => {
+                new Promise((resolve, reject) => {
 
-                        console.log('ENTER PROMISE', user_session[from_id].location)
-                        console.log(user_session[from_id].location)
+                    console.log('ENTER PROMISE', user_session[from_id].location)
+                    console.log(user_session[from_id].location)
 
-                        services.getTheaters(user_session[from_id].location, resolve, reject)
+                    services.getTheaters(user_session[from_id].location, resolve, reject)
 
-                    }).then((list_theaters) => {
+                }).then((list_theaters) => {
 
-                        console.log('EXIT PROMISE')
-                        console.log('list_theaters', list_theaters)
-
-                    })
-                }
+                    console.log('EXIT PROMISE')
+                    console.log('list_theaters', list_theaters)
 
 
+                    // if (data.length > 0){
+                    //     events.returnTheaters(
+                    //         token,
+                    //         user_session[sender_id].id,
+                    //         list_theaters
+                    //     );
+                    //     user_session[from_id].status = STATUSES.THEATERS_RECEIVED;
+                    // }
+
+                })
 
             }
 
